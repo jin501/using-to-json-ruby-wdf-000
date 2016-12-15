f1f795fed18781a0d3ff1596415eb1b744b2fa13
jin501 <soljin01@gmail.com>
2016-12-15 14:59:22 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,16 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post.to_json(only: [:title, :description, :id], include: [author: { only: [:name]}]) }
+    end
   end
 
   def new
     @post = Post.new
+    @action = "Submit"
   end
 
   def create
@@ -19,6 +25,7 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @action = "Update"
   end
 
   def update
@@ -28,7 +35,7 @@ class PostsController < ApplicationController
 
   def post_data
     post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
+    render json: post.to_json(only: [:title, :description, :id], include: [author: { only: [:name]}])
   end
 
 private
